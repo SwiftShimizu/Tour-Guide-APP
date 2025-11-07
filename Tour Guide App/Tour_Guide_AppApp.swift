e2d962908e7a27ad42fb4c4daa54f171f1900830
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2025-11-08 07:59:32 +0900

    Initial Commit

---
+++ Tour Guide App/Tour_Guide_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tour_Guide_AppApp.swift
+//  Tour Guide App
+//
+//  Created by 志水拓哉 on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct Tour_Guide_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
